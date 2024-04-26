d7c6b8dc4cad0a8e415deb2d66a004c5902bf8cd
dhia <dhiasmairi123@gmail.com>
2024-04-26 14:49:16 +0100

    add collocation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.compo
 import { DashboardUserComponent } from './dashboard-user/dashboard-user.component';
 import { LoginComponent } from './login/login.component';
 import { MapsComponent } from './dashboard-user/maps/maps.component';
+import { AddCollocationComponent } from './dashboard-user/add-collocation/add-collocation.component';
 
 export const routes: Routes = [
     { path: 'dashboardAdmin', component: DashboardAdminComponent },
     { path: 'dashboardUser', component: DashboardUserComponent , children:[
-        {path: 'Maps', component:MapsComponent}, ]},
+        {path: 'Maps', component:MapsComponent},
+        { path: 'add-collocation', component:AddCollocationComponent }, 
+     ]},
     { path: 'login', component: LoginComponent },
     {path: '', redirectTo:'login', pathMatch: 'full'},
     
